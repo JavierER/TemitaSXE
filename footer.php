2c9759dae5d42f5c8af2efd53b6fe3fca59e1596
Javier Estevez <jestevezrial@danielcastelao.org>
2016-12-11 23:03:19 +0000

    añadido footer

---
+++ footer.php
@@ -0,0 +1,24 @@
+<div id="footer">
+   <div id="fondofooter">
+    <ul>
+        <li>
+            <img src="<?php echo get_template_directory_uri() ?>/images/twitter.png" ALT="twitter">
+        </li>
+        <li>
+            <img src="<?php echo get_template_directory_uri() ?>/images/face.png" ALT="face">
+        </li>
+        <li>
+            <img src="<?php echo get_template_directory_uri() ?>/images/yt.png" ALT="yt">
+        </li>
+    </ul>
+    
+    </div> 
+
+    <div id="textofooter">
+        <h1>ESTO ES UN FOOTER.PHP</h1>
+    </div>
+    
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
